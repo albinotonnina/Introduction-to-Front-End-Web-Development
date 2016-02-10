cd21bfc372140d625df6f3a702394c032041a4d2
Albino Tonnina <albino.tonnina@workshare.com>
2016-02-10 22:38:04 +0000

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /* global module:false */
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     var port = grunt.option('port') || 8000;
     var base = grunt.option('base') || '.';
 
@@ -7,8 +7,14 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         meta: {
-            banner: '/*!\n' +
+            banner:
+            '/*!\n' +
+
             ' */'
+        },
+
+        qunit: {
+            files: [ 'test/*.html' ]
         },
 
         uglify: {
@@ -49,7 +55,7 @@ module.exports = function (grunt) {
         cssmin: {
             compress: {
                 files: {
-                    'css/reveal.min.css': ['css/reveal.css']
+                    'css/reveal.min.css': [ 'css/reveal.css' ]
                 }
             }
         },
@@ -76,7 +82,7 @@ module.exports = function (grunt) {
                     exports: false
                 }
             },
-            files: ['Gruntfile.js', 'js/reveal.js']
+            files: [ 'Gruntfile.js', 'js/reveal.js' ]
         },
 
         connect: {
@@ -84,7 +90,7 @@ module.exports = function (grunt) {
                 options: {
                     port: port,
                     base: base,
-                    livereload: 35728,
+                    livereload: true,
                     open: true
                 }
             }
@@ -97,71 +103,70 @@ module.exports = function (grunt) {
                 'js/**',
                 'lib/**',
                 'images/**',
-                'plugin/**'
+                'plugin/**',
+                '**.md'
             ]
         },
 
         watch: {
             options: {
-                livereload: 35728
+                livereload: true
             },
             js: {
-                files: ['Gruntfile.js', 'js/*.js'],
+                files: [ 'Gruntfile.js', 'js/reveal.js' ],
                 tasks: 'js'
             },
             theme: {
-                files: ['css/theme/source/*.scss', 'css/theme/template/*.scss'],
+                files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
                 tasks: 'css-themes'
             },
             css: {
-                files: ['css/reveal.scss'],
+                files: [ 'css/reveal.scss' ],
                 tasks: 'css-core'
             },
-            cssdemo: {
-                files: ['css/demo.css']
-            },
             html: {
-                files: ['*/*.html']
+                files: [ 'index.html']
             },
-            json: {
-                files: ['steps/*.json']
+            markdown: {
+                files: [ './*.md' ]
             }
         }
 
     });
 
     // Dependencies
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-sass' );
+    grunt.loadNpmTasks( 'grunt-contrib-connect' );
+    grunt.loadNpmTasks( 'grunt-autoprefixer' );
+    grunt.loadNpmTasks( 'grunt-zip' );
 
     // Default task
-    grunt.registerTask('default', ['css', 'js']);
+    grunt.registerTask( 'default', [ 'css', 'js' ] );
 
     // JS task
-    grunt.registerTask('js', ['jshint', 'uglify']);
+    grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
 
     // Theme CSS
-    grunt.registerTask('css-themes', ['sass:themes']);
+    grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
 
     // Core framework CSS
-    grunt.registerTask('css-core', ['sass:core', 'autoprefixer', 'cssmin']);
+    grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
 
     // All CSS
-    grunt.registerTask('css', ['sass', 'autoprefixer', 'cssmin']);
+    grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
 
     // Package presentation to archive
-    grunt.registerTask('package', ['default', 'zip']);
+    grunt.registerTask( 'package', [ 'default', 'zip' ] );
 
     // Serve presentation locally
-    grunt.registerTask('serve', ['connect', 'watch']);
+    grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
 
     // Run tests
-    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
 
 };
